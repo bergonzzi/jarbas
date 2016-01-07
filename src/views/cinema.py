4fc4a2cef8203f51689d6508920a804ea8710e25
André Bergonse <bergonzzi@gmail.com>
2016-01-07 21:11:09 +0100

    Improved cinema help

--- src/views/cinema.py
+++ src/views/cinema.py
@@ -13,8 +13,8 @@ class CinemaViews(object):
     def get_movies(self, message, match):
         arg = match.group('cinema').lower()
 
-        if arg in ('lista', 'list', 'ajuda', 'help'):
-            msg = 'Podes ver as sessões disponíveis em qualquer um destes cinemas (por ex. /cinema alegro):\n\n'
+        if arg in ('lista', 'list', 'ajuda', 'help', 'salas'):
+            msg = 'Podes ver as sessões disponíveis em qualquer um destes cinemas (podes usar um nome mais curto, por ex. /cinema alegro):\n\n'
 
             for k, v in config.cinema_sources.iteritems():
                 msg += v['name'] + '\n'
