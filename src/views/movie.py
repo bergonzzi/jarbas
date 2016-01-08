659e134e5e863794939cc511fa6d89bf436d468d
André Bergonse <bergonzzi@gmail.com>
2016-01-08 22:00:36 +0100

    Improvements to movie api

--- src/views/movie.py
+++ src/views/movie.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+import logging
+import datetime
 from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
 from tmdb3 import set_key, set_cache, searchMovie, Movie
-import numbers
 import config
 
 
@@ -10,29 +11,37 @@ class MovieViews(object):
         self.movie_list = []
         self.api_key = set_key(config.tmdb_api_key)
         self.routes = [
-            ('^' + config.cmd_prefix + '(?:searchmovie|(?:procura|pesquisa)r?\s?filme)\s(?P<movie>[^$]+)$', self.search_movie),
+            ('^' + config.cmd_prefix + '(?:searchmovie|(?:procura|pesquisa)r?\s?filmes?)\s(?P<movie>[^$]+)$', self.search_movie),
             ('^' + config.cmd_prefix + '(?:movie|filme)\s(?P<movie>[^$]+)$', self.get_movie)
         ]
 
         set_cache(filename=config.movie_cache)
 
-    def _get_movie_details(self, results):
+    def _get_movie_details(self, movie_id):
         try:
-            movie = results[0]
+            movie = Movie(movie_id)
             title = movie.title.encode('utf-8')
             overview = movie.overview.encode('utf-8')
-            releasedate = movie.releasedate.strftime('%Y-%m-%d').encode('utf-8')
-            trailer = '\n\nTrailer: %s' % Movie(movie.id).youtube_trailers[0].geturl()
 
-            if movie.tagline.encode('utf-8'):
+            if not overview:
+                overview = 'N/A'
+
+            if movie.tagline:
                 tagline = 'Tagline: %s\n\n' % movie.tagline.encode('utf-8')
             else:
                 tagline = ''
 
-            if not trailer:
+            try:
+                trailer = '\n\nTrailer: %s' % movie.youtube_trailers[0].geturl()
+            except IndexError:
                 trailer = ''
 
-            msg = '%s (%s)\n\n%sOverview: %s%s' % (title, releasedate, tagline, overview, trailer)
+            if movie.releasedate:
+                releasedate = ' (' + movie.releasedate.strftime('%Y') + ')'
+            else:
+                releasedate = ''
+
+            msg = '%s%s\n\n%sOverview: %s%s' % (title, releasedate, tagline, overview, trailer)
         except IndexError:
             msg = False
 
@@ -55,22 +64,24 @@ class MovieViews(object):
             msg = self._not_found(match.group('movie'))
         elif len(res) == 1:
             # If only 1 result show the details immediately
-            msg = self._get_movie_details(res)
+            movie_id = res[0].id
+            msg = self._get_movie_details(movie_id)
         else:
             for i, movie in enumerate(res):
                 num = i + 1
 
                 if num < config.max_movies:
+                    movie_id = movie.id
                     title = movie.title.encode('utf-8')
 
-                    # Sometimes a unicode object is returned instead of a datetime
-                    try:
-                        releasedate = movie.releasedate.strftime('%Y').encode('utf-8')
-                    except AttributeError:
-                        releasedate = movie.releasedate.encode('utf-8')
+                    # Some movies have no date
+                    if movie.releasedate:
+                        releasedate = ' (' + movie.releasedate.strftime('%Y') + ')'
+                    else:
+                        releasedate = ''
 
-                    msg += '%d. %s (%s)\n' % (num, title, releasedate)
-                    self.movie_list.append(title)
+                    msg += '%d. %s%s\n' % (num, title, releasedate)
+                    self.movie_list.append(movie_id)
                 elif num == config.max_movies + 1:
                     more_results = '...'
 
@@ -86,14 +97,17 @@ class MovieViews(object):
             movie_num = int(arg) - 1
 
             try:
-                movie_title = self.movie_list[movie_num]
-                msg = self._get_movie_details(searchMovie(movie_title))
+                movie_id = self.movie_list[movie_num]
+                msg = self._get_movie_details(movie_id)
             except IndexError:
                 msg = 'O número %d não está na lista!' % (movie_num + 1)
 
         # Otherwise do a search by title
         else:
-            msg = self._get_movie_details(searchMovie(arg))
+            try:
+                msg = self._get_movie_details(searchMovie(arg)[0].id)
+            except IndexError:
+                msg = self._not_found(match.group('movie'))
 
         if not msg:
             msg = self._not_found(match.group('movie'))
